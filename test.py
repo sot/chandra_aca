9989a4c858af793032d5364119dc8110b4945796
Tom Aldcroft <taldcroft@gmail.com>
2015-03-12 16:32:15 -0400

    Update test.py and add NOTES.test

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-from nose.tools import *
 import numpy as np
 from astropy.io import ascii
 
-import Chandra.ACA
+import chandra_aca
 
 TOLERANCE = 0.05
 
@@ -12,7 +11,7 @@ def test_pix_to_angle():
 
     print "testing {} row/col pairs match to {} arcsec".format(
         len(pix_to_angle), TOLERANCE)
-    pyyang, pyzang = Chandra.ACA.pixels_to_yagzag(
+    pyyang, pyzang = chandra_aca.pixels_to_yagzag(
         pix_to_angle['row'],
         pix_to_angle['col'])
     np.testing.assert_allclose(pix_to_angle['yang'], pyyang, atol=TOLERANCE)
@@ -23,7 +22,7 @@ def test_angle_to_pix():
     angle_to_pix = ascii.read(open('test_data/angle_to_pix.txt'))
     print "testing {} yang/zang pairs match to {} pixels".format(
         len(angle_to_pix), TOLERANCE)
-    pyrow, pycol = Chandra.ACA.yagzag_to_pixels(
+    pyrow, pycol = chandra_aca.yagzag_to_pixels(
         angle_to_pix['yang'],
         angle_to_pix['zang'])
     np.testing.assert_allclose(angle_to_pix['row'], pyrow, atol=TOLERANCE)
