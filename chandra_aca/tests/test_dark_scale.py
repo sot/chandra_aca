ea504e682263bb6c7681bf690bed8a34e0ee1612
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-02-06 09:24:06 -0500

    Update path to dark_temp_scale in test

--- chandra_aca/tests/test_dark_scale.py
+++ chandra_aca/tests/test_dark_scale.py
@@ -1,9 +1,9 @@
 import numpy as np
-from .. import dark_scale
+from ..dark_model import dark_temp_scale
 
 def test_dark_temp_scale():
-    scale = dark_scale.dark_temp_scale(-10., -14)
+    scale = dark_temp_scale(-10., -14)
     assert np.allclose(scale, 0.70)
 
-    scale = dark_scale.dark_temp_scale(-10., -14, scale_4c=2.0)
+    scale = dark_temp_scale(-10., -14, scale_4c=2.0)
     assert scale == 0.5  # Should be an exact match
