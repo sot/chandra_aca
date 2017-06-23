92b9b33a13b093d1a9bf6ac22b000405a3403234
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-06-23 17:00:44 -0400

    Update tests using helper method for new API

--- chandra_aca/tests/test_residuals.py
+++ chandra_aca/tests/test_residuals.py
@@ -3,16 +3,16 @@ from ..centroid_resid import get_obs_slot_residuals
 
 def test_multi_ai():
     # obsid 15175 has two aspect intervals
-    dyags, dzags = get_obs_slot_residuals(15175, 4)
+    dyags, yt, dzags, zt = get_obs_slot_residuals(15175, 4)
     assert np.all(np.abs(dyags) < 3)
     assert np.all(np.abs(dzags) < 6)
 
 def test_obc_centroids():
-    dyags, dzags = get_obs_slot_residuals(15175, 5, centroid_source='obc')
+    dyags, yt, dzags, zt = get_obs_slot_residuals(15175, 5, centroid_source='obc')
     assert np.all(np.abs(dyags) < 6)
     assert np.all(np.abs(dzags) < 3)
 
 def test_obc():
-    dyags, dzags = get_obs_slot_residuals(15175, 6, att_source='obc', centroid_source='obc')
+    dyags, yt, dzags, zt = get_obs_slot_residuals(15175, 6, att_source='obc', centroid_source='obc')
     assert np.all(np.abs(dyags) < 4.5)
     assert np.all(np.abs(dzags) < 5.5)
