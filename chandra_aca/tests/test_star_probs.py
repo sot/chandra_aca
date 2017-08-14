95bb158c30318cc6ad3b1a5d7b84f1c15311bfbe
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-08-14 14:53:06 -0400

    Update regress vals for expected vals from 3.16.1 model

--- chandra_aca/tests/test_star_probs.py
+++ chandra_aca/tests/test_star_probs.py
@@ -6,19 +6,19 @@ from chandra_aca.star_probs import t_ccd_warm_limit, mag_for_p_acq, acq_success_
 
 def test_t_ccd_warm_limit_1():
     out = t_ccd_warm_limit([10.4] * 6, date='2015:001', min_n_acq=(2, 8e-3))
-    assert np.allclose(out[0], -14.7541, atol=0.01, rtol=0)
+    assert np.allclose(out[0], -14.9924, atol=0.01, rtol=0)
     assert np.allclose(out[1], 0.008, atol=0.0001, rtol=0)
 
 
 def test_t_ccd_warm_limit_2():
     out = t_ccd_warm_limit([10.4] * 6, date='2015:001', min_n_acq=5.0)
-    assert np.allclose(out[0], -14.609, atol=0.01, rtol=0)
+    assert np.allclose(out[0], -14.851, atol=0.01, rtol=0)
     assert np.allclose(out[1], 5.0, atol=0.01, rtol=0)
 
 
 def test_mag_for_p_acq():
     mag = mag_for_p_acq(0.50, date='2015:001', t_ccd=-14.0)
-    assert np.allclose(mag, 10.867, rtol=0, atol=0.01)
+    assert np.allclose(mag, 10.848, rtol=0, atol=0.01)
 
 
 def test_halfwidth_adjustment():
@@ -27,23 +27,23 @@ def test_halfwidth_adjustment():
     p120 = acq_success_prob(mag=mag, halfwidth=120)
     pacq = acq_success_prob(mag=mag, halfwidth=halfwidth)
     mults = pacq / p120
-    assert np.allclose(mults, [1.07721523, 1.04776216, 1., 0.90920264, 0.83013465])
+    assert np.allclose(mults, [1.06987729,  1.0438542 ,  1.,  0.91504106,  0.83987265])
 
 
 def test_acq_success_prob_date():
     date = ['2014:001', '2015:001', '2016:001', '2017:001']
     probs = acq_success_prob(date=date, t_ccd=-10, mag=10.3, spoiler=False, color=0.6)
-    assert np.allclose(probs, [0.78033749, 0.75681248, 0.73114306, 0.70326393])
+    assert np.allclose(probs, [0.76856955,  0.74345895,  0.71609812,  0.68643974])
 
 def test_acq_success_prob_t_ccd():
     t_ccd = [-16, -14, -12, -10]
     probs = acq_success_prob(date='2017:001', t_ccd=t_ccd, mag=10.3, spoiler=False, color=0.6)
-    assert np.allclose(probs, [0.87533264,  0.82770406,  0.77013684,  0.70326393])
+    assert np.allclose(probs, [0.87007558,  0.81918958,  0.75767782,  0.68643974])
 
 def test_acq_success_prob_mag():
     mag = [9, 10, 10.3, 10.6]
     probs = acq_success_prob(date='2017:001', t_ccd=-10, mag=mag, spoiler=False, color=0.6)
-    assert np.allclose(probs, [0.985,  0.88094708,  0.70326393,  0.41512498])
+    assert np.allclose(probs, [ 0.985,  0.86868674,  0.68643974,  0.3952578])
 
 
 def test_acq_success_prob_spoiler():
@@ -57,5 +57,5 @@ def test_acq_success_prob_color():
     p_0p7color = .4294  # probability multiplier for a B-V = 0.700 star (REF?)
     color = [0.6, 0.7, 1.5]
     probs = acq_success_prob(date='2017:001', t_ccd=-10, mag=10.3, spoiler=False, color=color)
-    assert np.allclose(probs, [0.70326393,  0.30198153,  0.35834248])
+    assert np.allclose(probs, [ 0.68643974,  0.29475723,  0.29295036])
     assert np.allclose(p_0p7color, probs[1] / probs[0])
