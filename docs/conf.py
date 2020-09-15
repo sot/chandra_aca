e3af5d53b4d2f40e7a06189abcd929927bd206bf
Javier Gonzalez <javier.gonzalez@cfa.harvard.edu>
2020-09-15 14:49:00 -0400

    ska theme

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,12 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'classic'
+html_theme = 'bootstrap-ska'
+html_theme_options = {
+    'logotext1': 'Ska! ',  # white,  semi-bold
+    'logotext2': 'chandra_aca',  # orange, light
+    'logotext3': '',   # white,  light
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
