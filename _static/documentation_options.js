7441da747b651057744300563d8c4ca7da13b7af
Javier Gonzalez <javier.gonzalez@cfa.harvard.edu>
2020-09-22 17:28:35 -0400

    docs update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.29.2.dev22+g7878cb9',
+    VERSION: '4.29.2.dev23+gac11754',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
