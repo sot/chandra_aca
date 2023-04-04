27dd695683973359041ceaa0ef420b44eda67dd9
Javier Gonzalez <javierggt@yahoo.com>
2023-04-04 13:16:43 -0400

    black

--- chandra_aca/tests/test_maude_decom.py
+++ chandra_aca/tests/test_maude_decom.py
@@ -776,8 +776,8 @@ def test_imgtype_dnld():
         calibrate=False,
     )
 
-    all_masked = np.array([np.all(row['IMG'].mask) for row in img])
-    img_dnld = (img['IMGTYPE'] == 3)
+    all_masked = np.array([np.all(row["IMG"].mask) for row in img])
+    img_dnld = img["IMGTYPE"] == 3
     assert np.all(all_masked == img_dnld)
 
     # what is tested still needs to be determined, but at the very least it should not fail.
